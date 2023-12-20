bd310927fe3086f41daa36f95d8e7c71080556ad
Pirathikaran <Pirathikaran.v@eyepax.com>
2023-12-20 01:39:35 -0300

    Created task manager app

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {  useState, useEffect } from 'react';
+import { AddTask } from './components/AddTask';
+import { Header } from './components/Header';
+import { ShowTask } from './components/ShowTask';
+
+import './App.css';
+
+function App() {
+  const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem("tasklist")) || []);
+  const [task, setTask] = useState({});
+
+  useEffect(()=>{
+    localStorage.setItem("tasklist", JSON.stringify(taskList))
+  },[taskList]);
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <AddTask 
+      taskList={taskList} 
+      setTaskList={setTaskList}
+      task={task}
+      setTask={setTask}
+      ></AddTask>
+      <ShowTask 
+      taskList={taskList} 
+      setTaskList={setTaskList}
+      task={task}
+      setTask={setTask}
+      ></ShowTask>
+    </div>
+  );
+}
+
+export default App;
